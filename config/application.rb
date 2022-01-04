0fbb1929ee41478e1fb876aca661739190f9cd79
Umer Hassam <umer.hassam@gmail.com>
2022-01-04 17:14:07 +0500

    fixing push fail 8

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module RecruitmentPortal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,6 +15,8 @@ module RecruitmentPortal
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.assets.initialize_on_precompile = false
   end
 end
 
