aaab9744bac66c5cc0abe2026f090d77dfc557e4
Umer Hassam <umerhassam@Zainabs-MacBook-Pro.local>
2022-10-10 11:55:28 +0500

    added employee_rate and employee_id to migration

--- db/migrate/20221005131854_add_job_columns_to_consultant.rb
+++ db/migrate/20221005131854_add_job_columns_to_consultant.rb
@@ -3,10 +3,10 @@ class AddJobColumnsToConsultant < ActiveRecord::Migration[6.0]
     add_column :employees, :job_start_date, :date
     add_column :employees, :job_end_date,   :date
     add_column :employees, :job_end_reason, :string
-    #add_column :employees, :employee_rate, :decimal
+    add_column :employees, :employee_rate, :decimal
 
     add_column :work_durations, :job_id, :integer
-    #add_column :work_durations, :employee_id, :integer
+    add_column :work_durations, :employee_id, :integer
 
   end
 end
