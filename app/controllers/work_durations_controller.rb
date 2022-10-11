60fef53411a61655b02296c9d0ea7039f21dc5cd
Umer Hassam <umerhassam@Zainabs-MacBook-Pro.local>
2022-10-11 13:04:55 +0500

    hello

--- app/controllers/work_durations_controller.rb
+++ app/controllers/work_durations_controller.rb
@@ -345,12 +345,16 @@ class WorkDurationsController < ApplicationController
           end
 
         elsif !wd.project.blank? && !wd.project.employee.blank?
-          if !wd.project.job.blank? 
+          if !wd.project.job.blank?
+
+            job = wd.project.employee.active_job
+
             if wd.update(employer_rate: wd.project.employee.employer_rate, 
                           consultant_rate: wd.project.employee.employee_rate,
-                          job_rate: wd.project.job.rate,
-                          job_id: wd.project.job_id,
+                          job_rate: job.rate,
+                          job_id: job.id,
                           employee_id: wd.project.employee_id)
+            
               print "--> UPDATING SUCCESS"
               flash[:notice] = "Updated successfully."
             else
