532e771b14ddc5566c6e1f7012380d25681937f2
SherazAmin <m.sheraz.amin1@gmail.com>
2023-06-06 22:42:15 +0500

    Update production.rb
    added :login as authentication type to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,13 +51,13 @@ Rails.application.configure do
   config.log_level = :debug
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:                'smtp.office365.com',
-    port:                   587,
-    user_name:              'contact@vertexspecial.com',
-    domain:                 'vertexspecial.com',
-    password:               'Vertex@123**',
-    authentication:         'plain',
-    tls:                    true
+    :address         => 'smtp.office365.com',
+    :port            => 587,
+    :user_name       => 'contact@vertexspecial.com',
+    :domain          => 'vertexspecial.com',
+    :password        => 'Vertex@123**',
+    :authentication  => :login,
+    :tls             => true
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
