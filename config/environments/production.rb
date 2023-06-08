57bbd8ec87f21db97a3cf543a41490269ecd713a
SherazAmin <m.sheraz.amin1@gmail.com>
2023-06-08 21:11:24 +0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.default_url_options = { :host => 'https://stark-springs-55450.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'vertexspecial.com' }
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
